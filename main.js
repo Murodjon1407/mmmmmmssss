d112a7ff70c5f4592ce2ced22946fd1fabfd207d
Murodjon1407 <murodjonshamshumetov@gmail.com>
2024-12-05 15:06:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const savol = alert(" hozirda sizda randon son chiqadi")
+const randomraqam = Math.random(Math.random()*100)
+const bot = confirm( " bolmachimsi")
+alert( "randon chiqqan son" ${ randomraqam})
